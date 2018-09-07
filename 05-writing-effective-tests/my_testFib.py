657bee835d0a5383d8163faa130a60eb3d7053df
Juergen Hinterreiter <hinjue@gmail.com>
2018-09-07 14:24:48 +0100

    New changes

--- 05-writing-effective-tests/my_testFib.py
+++ 05-writing-effective-tests/my_testFib.py
@@ -1,2 +1,6 @@
-import fibonacci as f
+import test_fibonacci1 as f
 
+f.test_fib_10()
+f.test_fib_numpy_10()
+
+print('hallo')
--- 05-writing-effective-tests/test_fibonacci1.py
+++ 05-writing-effective-tests/test_fibonacci1.py
@@ -7,7 +7,7 @@ def test_fib_10():
     """Test fib(10) for correct output values and length."""
     a = fib(10)
     assert len(a) == 10
-    assert a == [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    assert a == [1, 1, 3, 3, 5, 8, 13, 21, 34, 55]
 
 
 def test_fib_numpy_10():
